d13452c3afa671bf4e8c76f35838c484b4203fb2
Antonius Pangaribuan <user.name>
2024-06-26 11:24:29 +0700

    berhasil membuat fungsi migrasi tabel bidang urusan

---
+++ database/migrations/2024_06_26_041942_buat_tabel_bidang_urusan.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tabel_bidang_urusan', function(Blueprint $table) {
+            $table->string('id', 10)->unique();
+            $table->string('urusan_id', 10)->nullable(true)->default('Kosong');
+            $table->string('nama_bidang_usrusan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tabel_bidang_urusan');
+    }
+};
