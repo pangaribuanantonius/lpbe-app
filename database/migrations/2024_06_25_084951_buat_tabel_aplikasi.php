3ff8a44a7aa2354bf22fa3bd407d57eb7b793d4e
Antonius Pangaribuan <user.name>
2024-06-26 09:23:44 +0700

    berhasil membuat migrasi tabel aplikasi

---
+++ database/migrations/2024_06_25_084951_buat_tabel_aplikasi.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tabel_aplikasi', function(Blueprint $table) {
+            $table->string('id', 10)->unique();
+            $table->string('instansi_id', 10)->nullable(true)->default('Kosong');
+            $table->string('urusan_id', 10)->nullable(true)->default('Kosong');
+            $table->string('bidang_urusan_id', 10)->nullable(true)->default('Kosong');
+            $table->string('nama_aplikasi')->nullable(true)->default('Kosong');
+            $table->string('kepemilikan')->nullable(true)->default('Kosong');
+            $table->string('tahun_digunakan')->nullable(true)->default('Kosong');
+            $table->longText('dasarhukum')->nullable(true);
+            $table->string('tahun_pembuatan')->nullable(true)->default('Kosong');
+            $table->string('launching')->nullable(true)->default('Kosong');
+            $table->string('desktop')->nullable(true)->default('Kosong');
+            $table->string('web')->nullable(true)->default('Kosong');
+            $table->string('android')->nullable(true)->default('Kosong');
+            $table->string('ios')->nullable(true)->default('Kosong');
+            $table->string('platform')->nullable(true)->default('Kosong');
+            $table->string('platform2')->nullable(true)->default('Kosong');
+            $table->string('url')->nullable(true)->default('Kosong');
+            $table->string('tempataplikasi')->nullable(true)->default('Kosong');
+            $table->string('sektorpelayananpublik')->nullable(true)->default('Kosong');
+            $table->longText('deskripsi')->nullable(true);
+            $table->longText('daftarlayanan')->nullable(true);
+            $table->longText('daftarproduklayanan')->nullable(true);
+            $table->string('pengguna')->nullable(true)->default('Kosong');
+            $table->string('nama_pic')->nullable(true)->default('Kosong');
+            $table->string('jabatan_pic')->nullable(true)->default('Kosong');
+            $table->string('kontak')->nullable(true)->default('Kosong');
+            $table->string('tahun')->nullable(true)->default('Kosong');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
