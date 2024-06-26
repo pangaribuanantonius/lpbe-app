3c7cbbb44c437eb8d1a4d707b87e7c9592cce852
Antonius Pangaribuan <user.name>
2024-06-26 09:50:20 +0700

    berhasil buat seeder aplikasi dan perbaikan migrasi aplikasi

--- database/migrations/2024_06_26_024232_buat_tabel_aplikasi.php
+++ database/migrations/2024_06_26_024232_buat_tabel_aplikasi.php
@@ -47,6 +47,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('tabel_aplikasi');
     }
 };
---
+++ database/seeders/AplikasiSeeder.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AplikasiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $listaplikasi = [
+            [
+                'id' => 'rUiLbk8w',
+                'instansi_id' => 'Kosong',
+                'urusan_id' => 'Kosong',
+                'bidang_urusan_id' => 'Kosong',
+                'nama_aplikasi' => 'Test',
+                'kepemilikan' => 'Kosong',
+                'tahun_digunakan' => '2000',
+                'dasarhukum' => 'Kosong',
+                'tahun_pembuatan' => '2000',
+                'launching' => 'Kosonng',
+                'desktop' => 'Kosong',
+                'web' => 'Kosong',
+                'android' => 'Kosong',
+                'ios' => 'Kosong',
+                'platform' => 'Kosong',
+                'platform2' => 'Kosong',
+                'url' => 'https://portal.deliserdangkab.go.id',
+                'tempataplikasi' => 'Kosong',
+                'sektorpelayananpublik' => 'Kosong',
+                'deskripsi' => 'Kosong',
+                'daftarlayanan' => 'Kosong',
+                'daftarlayanan' => 'Kosong',
+                'pengguna' => 'Kosong',
+                'nama_pic' => 'Kosong',
+                'jabatan_pic' => 'Kosong',
+                'kontak' => 'Kosong',
+                'tahun' => 'tahun', 
+            ],
+            [
+                'id' => 'ewffd9kZ',
+                'instansi_id' => 'Kosong',
+                'urusan_id' => 'Kosong',
+                'bidang_urusan_id' => 'Kosong',
+                'nama_aplikasi' => 'Test',
+                'kepemilikan' => 'Kosong',
+                'tahun_digunakan' => '2000',
+                'dasarhukum' => 'Kosong',
+                'tahun_pembuatan' => '2000',
+                'launching' => 'Kosonng',
+                'desktop' => 'Kosong',
+                'web' => 'Kosong',
+                'android' => 'Kosong',
+                'ios' => 'Kosong',
+                'platform' => 'Kosong',
+                'platform2' => 'Kosong',
+                'url' => 'https://portal.deliserdangkab.go.id',
+                'tempataplikasi' => 'Kosong',
+                'sektorpelayananpublik' => 'Kosong',
+                'deskripsi' => 'Kosong',
+                'daftarlayanan' => 'Kosong',
+                'daftarlayanan' => 'Kosong',
+                'pengguna' => 'Kosong',
+                'nama_pic' => 'Kosong',
+                'jabatan_pic' => 'Kosong',
+                'kontak' => 'Kosong',
+                'tahun' => 'tahun',
+            ],
+        ];
+        \DB::table('tabel_aplikasi')->insert($listaplikasi);
+    }
+}
