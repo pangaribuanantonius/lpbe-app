fdaf4db9202dd295633e9966a6f36c24da9d50c0
Antonius Pangaribuan <user.name>
2024-06-26 14:45:41 +0700

    berhasil membuat fungsi migrasi tabel kecamatan

---
+++ database/migrations/2024_06_26_074118_buat_tabel_kecamatan.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tabel_kecamatan', function(Blueprint $table){
+            $table->string('id', 10)->unique();
+            $table->string('nama_kecamatan')->nullable(true)->default('Kosong');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tabel_kecamatan');
+    }
+};
