4fd3b3470602bf92efe5f41e7667255d982eba08
Antonius Pangaribuan <user.name>
2024-06-26 10:36:32 +0700

    berhasil buat data seeder call center

--- database/seeders/CallCenterSeeder.php
+++ database/seeders/CallCenterSeeder.php
@@ -12,6 +12,42 @@ class CallCenterSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $listcallcenter = [
+            [
+                 'id' => 'nsFmwxDw',
+                 'instansi_id' => 'Kosong',
+                 'nama_layanan' => 'Kosong',
+                 'nomor_layanan' => 'Kosong',
+                 'deskripsi_layanan' => 'Kosong',
+                 'whatsapp' => 'Kosong',
+                 'telepon' => 'Kosong',
+                 'platform' => 'Kosong',
+                 'sektorlayanan' => 'Kosong',
+                 'sektorlainnya' => 'Kosong',
+                 'nama_pic' => 'Kosong',
+                 'jabatan_pic' => 'Kosong',
+                 'kontak' => 'Kosong',
+                 'Tahun' => '2000',
+                 'Status' => 'Kosong',
+            ],
+            [
+                'id' => 'oFLXFE1r',
+                 'instansi_id' => 'Kosong',
+                 'nama_layanan' => 'Kosong',
+                 'nomor_layanan' => 'Kosong',
+                 'deskripsi_layanan' => 'Kosong',
+                 'whatsapp' => 'Kosong',
+                 'telepon' => 'Kosong',
+                 'platform' => 'Kosong',
+                 'sektorlayanan' => 'Kosong',
+                 'sektorlainnya' => 'Kosong',
+                 'nama_pic' => 'Kosong',
+                 'jabatan_pic' => 'Kosong',
+                 'kontak' => 'Kosong',
+                 'Tahun' => '2000',
+                 'Status' => 'Kosong',
+            ],
+        ];
+        \DB::table('tabel_callcenter')->insert($listcallcenter);
     }
 }
