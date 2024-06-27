49c6743bd6658d711742ebd012a2e80222fe67ae
Antonius Pangaribuan <user.name>
2024-06-27 14:10:44 +0700

    perbaikan migrasi tabel pemberitahuan

--- database/migrations/2024_06_27_070708_buat_tabel_pemberitahuan.php
+++ database/migrations/2024_06_27_070708_buat_tabel_pemberitahuan.php
@@ -13,9 +13,7 @@ return new class extends Migration
     {
         Schema::create('tabel_pemberitahuan', function(Blueprint $table) {
             $table->string('id', 10)->unique();
-            $table->string('bidang_urusan_id', 10)->nullable(true)->default('Kosong');
-            $table->string('nama_aplikasi')->nullable(true)->default('Kosong');
-            $table->string('kepemilikan')->nullable(true)->default('Kosong');
+            $table->longText('isi_pemberitahuan')->nullable(true);
             $table->timestamps();
         });
     }
