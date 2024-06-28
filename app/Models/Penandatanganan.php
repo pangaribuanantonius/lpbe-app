1fcf2271f6ccde0b27c4403c0805dac7cf709121
Antonius Pangaribuan <user.name>
2024-06-28 17:34:03 +0700

    membuat model penandatanganan dan perbaikan css sidebar penandatanganan level pengguna

---
+++ app/Models/Penandatanganan.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Penandatanganan extends Model
+{
+    use HasFactory;
+    protected $table = 'tabel_tandatangan_naskah';
+    protected $guarded = [];
+    public $incrementing = false;
+    public function instansi(){
+        return $this->belongsTo(Instansi::class, 'instansi_id', 'id');
+    }
+}
