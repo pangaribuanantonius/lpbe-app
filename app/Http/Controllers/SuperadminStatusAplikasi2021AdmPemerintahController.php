eafe329759ce648ba6045ed2d26bf1865368e43f
Antonius Pangaribuan <user.name>
2024-07-02 14:09:36 +0700

    membuat controller status aplikasi adm pemerintah 2021 level admin

---
+++ app/Http/Controllers/SuperadminStatusAplikasi2021AdmPemerintahController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Aplikasi;
+use App\Models\Instansi;
+
+use Illuminate\Http\Request;
+
+class SuperadminStatusAplikasi2021AdmPemerintahController extends Controller
+{
+    public function updatestatus(Request $request){
+        $instansi_id = $request->instansi_id;
+        $tahun = $request->tahun;
+        \App\Models\Aplikasi::Where('instansi_id', $instansi_id)->Where('jenis_aplikasi', 'Administrasi Pemerintah')->Where('tahun', '2021')->update([
+            'status' => $request->status,
+        ]);
+        return redirect('layanansuperadmin/index?layanan=aplikasi&jenisaplikasi=administrasi_pemerintah&tahun=2021&instansi_id='.request('instansi_id'))->with('updatestatus', 'Berhasil Memperbarui Data!');
+    }
+}
