bfb6359992f253c7a7af20f613aae5a3ea4afb9d
Antonius Pangaribuan <user.name>
2024-07-05 10:08:32 +0700

    perbaikan migrasi tabel berkas

--- database/migrations/2024_07_05_030646_buat_tabel_berkas.php
+++ database/migrations/2024_07_05_030646_buat_tabel_berkas.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExist('tabel_berkas');
+        Schema::dropIfExists('tabel_berkas');
     }
 };
