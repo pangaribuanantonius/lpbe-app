d8e02f40d09958d2bcf8bf0ac2723c1be54b4474
Antonius Pangaribuan <user.name>
2024-07-11 17:34:44 +0700

    perbaikan detail berkas aps level pengguna

--- resources/views/menu/detail_berkas.blade.php
+++ resources/views/menu/detail_berkas.blade.php
@@ -21,6 +21,7 @@
                             </tr>
                         </thead>
                         <tbody>
+                            @if($berkas->posisi == 'Pengguna')
                             <tr>
                                 <td>{{ $berkas->file_aps_publik }}</td>
                                 <td class="text-center">
@@ -42,6 +43,28 @@
                                     <a class="btn btn-outline-danger" href="{{ route('berkas.call_center.ubah_berkas', $berkas->id) }}"><i class="bi bi-pencil"></i>Ganti</a>
                                 </td>
                             </tr>
+                            @elseif($berkas->posisi == 'Admin')<tr>
+                                <td>{{ $berkas->file_aps_publik }}</td>
+                                <td class="text-center">
+                                    <a class="btn btn-outline-primary" target="_blank" href="{{ url('/')}}/konten/berkas/{{ $berkas->file_aps_publik }}"><i class="bi bi-eye"></i>Lihat</a>
+                                    
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>{{ $berkas->file_aps_pemerintah }}</td>
+                                <td class="text-center">
+                                    <a class="btn btn-outline-primary" target="_blank" href="{{ url('/')}}/konten/berkas/{{ $berkas->file_aps_pemerintah }}"><i class="bi bi-eye"></i>Lihat</a>
+                                    
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>{{ $berkas->file_call_center }}</td>
+                                <td class="text-center">
+                                    <a class="btn btn-outline-primary" target="_blank" href="{{ url('/')}}/konten/berkas/{{ $berkas->file_call_center }}"><i class="bi bi-eye"></i>Lihat</a>
+                                    
+                                </td>
+                            </tr>
+                            @endif
                         </tbody>
                     </table>
             
