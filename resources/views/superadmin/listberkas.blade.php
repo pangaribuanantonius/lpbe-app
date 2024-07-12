9f4cf8d42be1d308719903792de7671511e1a602
Antonius Pangaribuan <user.name>
2024-07-12 09:59:29 +0700

    racang tampilan berkas level admin

--- resources/views/superadmin/listberkas.blade.php
+++ resources/views/superadmin/listberkas.blade.php
@@ -19,6 +19,7 @@
             </thead>
             <tbody>
                 @foreach($listberkas as $bks)
+                @if($bks->posisi == 'Pengguna')
                 <tr>
                     <td>{{$bks->nama}}</td>
                     <td>{{$bks->tahun}}</td>
@@ -26,6 +27,16 @@
                         <a href="{{ route('superadmin.detail_berkas', $bks->id) }}" class="btn btn-outline-primary"><i class="bi bi-eye"></i> Lihat</a>
                     </td>
                 </tr>
+                @elseif($bks->posisi == 'Admin')
+                <tr>
+                    <td>{{$bks->nama}}</td>
+                    <td>{{$bks->tahun}}</td>
+                    <td class="text-center">
+                        <a href="{{ route('superadmin.detail_berkas', $bks->id) }}" class="btn btn-outline-primary"><i class="bi bi-eye"></i> Lihat</a>
+                        <a href="#" class="btn btn-outline-danger"><i class="bi bi-pencil"></i> Ubah</a>
+                    </td>
+                </tr>
+                @endif
                 @endforeach
             </tbody>
         </table>
