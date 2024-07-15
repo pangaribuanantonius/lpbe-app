58e193d047ed255bd89690022275db9398f76545
Antonius Pangaribuan <user.name>
2024-07-15 16:05:32 +0700

    tambah kolom verifikasi dan catatan pada tabel aplikasi

---
+++ database/migrations/2024_07_15_081747_tambah_kolom_verifikasi_admin_dan_keterangan_pada_tabel_aplikasi.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tabel_aplikasi', function (Blueprint $table) {
+            $table->string('verifikasi')->nullable(true)->default('Kosong')->after('status');
+            $table->string('catatan')->nullable(true)->default('Kosong')->after('verifikasi');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tabel_aplikasi', function (Blueprint $table) {
+            $table->dropColumn('verifikasi');
+            $table->dropColumn('catatan');
+        });
+    }
+};
--- resources/views/call_center/2021/cetaklaporanpdf.blade.php
+++ resources/views/call_center/2021/cetaklaporanpdf.blade.php
@@ -27,6 +27,7 @@
 				<th>Nomor Layanan</th>
 				<th>Deskripsi Layanan</th>
 				<th>Sektor Layanan</th>
+				<th>Nama Sektor Lain</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -36,6 +37,7 @@
 				<td>{{ $c->nomor_layanan }}</td>
 				<td>{{ $c->deskripsi_layanan }}</td>
 				<td>{{ $c->sektorlayanan }}</td>
+				<td>{{ $c->sektorlainnya }}</td>
 			</tr>
 			@endforeach
 		</tbody>
