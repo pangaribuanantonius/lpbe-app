ac1be72195756c7ba27a376665e5632a9b661c0e
Antonius Pangaribuan <user.name>
2024-08-22 15:25:52 +0700

    perbaikan view index aplikasi layanan publik 2024 level pengguna

--- resources/views/aplikasi/layanan_publik/2024/index.blade.php
+++ resources/views/aplikasi/layanan_publik/2024/index.blade.php
@@ -352,21 +352,21 @@
 									<p class="badge text-danger">
 										<i class="bi bi-x-circle"></i> {{ $aps->verifikasi }}
 									</p>
-									@elseif($aps->verifikasi == 'Disetujui')
-									<p class="badge text-success">
-										<i class="bi bi-check-circle"></i> {{ $aps->verifikasi }}
-									</p>
 									@elseif($aps->verifikasi == 'Ditinjau')
 									<p class="badge text-primary">
 										<i class="bi bi-info-circle"></i> {{ $aps->verifikasi }}
 									</p>
+									@elseif($aps->verifikasi == 'Disetujui')
+									<p class="badge text-success">
+										<i class="bi bi-check-circle"></i> {{ $aps->verifikasi }}
+									</p>
 									@elseif($aps->verifikasi == 'Kosong')
-									<p class="badge text-primary">
-										<i class="bi bi-info-circle"></i> Belum Diverifikasi
+									<p class="badge text-danger">
+										<i class="bi bi-ban"></i> Belum Diverifikasi
 									</p>
 									@else
-									<p class="badge text-primary">
-										<i class="bi bi-info-circle"></i> Belum Diverifikasi
+									<p class="badge text-danger">
+										<i class="bi bi-ban"></i> Belum Diverifikasi
 									</p>
 									@endif
 								</td>
