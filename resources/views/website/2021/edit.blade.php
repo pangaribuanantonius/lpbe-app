6ddcaae2b3af0d5f75e87766261edb3efe01b26b
Antonius Pangaribuan <user.name>
2024-09-04 11:54:51 +0700

    perbaikan view edit website 2021 level pengguna

--- resources/views/website/2021/edit.blade.php
+++ resources/views/website/2021/edit.blade.php
@@ -22,20 +22,20 @@
                         @method('POST')
                         <div class="form-group mt-3">
                             <label>Nama Website</label>
-                            <input type="text" name="nama_website" class="form-control" required>
+                            <input type="text" name="nama_website" value="{{ $website->nama_website }}" class="form-control" required>
                         </div>
                         <div class="form-group mt-3">
                             <label>Deskripsi Website</label>
-                            <textarea name="deskripsi_website" class="form-control"></textarea>
+                            <textarea name="deskripsi_website" class="form-control">{{ $website->deskripsi_website }}</textarea>
                         </div>
                         <div class="form-group mt-3">
                             <label>Alamat URL</label>
-                            <input type="url" name="url" class="form-control" required>
+                            <input type="url" name="url" class="form-control" value="{{ $website->url }}" required>
                         </div>
                         <div class="form-group mt-3">
                             <label>Pengembang</label>
                             <select name="pengembang" class="form-control" required>
-                                <option value="">-- Pilih --</option>
+                                <option value="{{$website->pengembang}}">{{ $website->pengembang }}</option>
                                 <option value="Dikembangkan Sendiri">Dikembangkan Sendiri</option>
                                 <option value="Dinas Komunikasi, Informatika, Statistik dan Persandian">Dinas Komunikasi, Informatika, Statistik dan Persandian</option>
                                 <option value="Pihak Ketiga">Pihak Ketiga</option>
@@ -44,28 +44,28 @@
                         <div class="form-group mt-3">
                             <label>Tempat</label>
                             <select name="tempat" class="form-control" required>
-                                <option value="">-- Pilih --</option>
+                                <option value="{{ $website->tempat }}">{{ $website->tempat }}</option>
                                 <option value="Unit Kerja Terkait">Unit Kerja Terkait</option>
                                 <option value="Dinas Komunikasi, Informatika, Statistik dan Persandian">Dinas Komunikasi, Informatika, Statistik dan Persandian</option>
                                 <option value="Pihak Ketiga">Pihak Ketiga</option>
                             </select>
                         </div>
                         <div class="form-group mt-3">
-                            <label>Apakah Website ini menyimpan data rahasia</label>
-                            <div>
-								<input type="radio" name="rahasia" id="ya" value="ya" required> Ya
+							<label>Apakah Website ini menyimpan data pribadi/rahasia ?</label>
+							<div>
+								<input type="radio" name="rahasia" id="ya" value="ya" {{ $website->rahasia === 'ya' ? 'checked' : '' }} required> Ya
 							</div>
 							<div>
-								<input type="radio" name="rahasia" id="tidak" value="tidak" required> Tidak
+								<input type="radio" name="rahasia" id="tidak" value="tidak" {{ $website->rahasia === 'tidak' ? 'checked' : '' }} required> Tidak
 							</div>
-                        </div>
+						</div>
                         <div class="form-group mt-3">
                             <label>Apakah Website ini menyediakan fitur ramah anak ?</label>
                             <div>
-								<input type="radio" name="ramah_anak" id="ya" value="ya" required> Ya
+								<input type="radio" name="ramah_anak" id="ya" value="ya" {{ $website->ramah_anak === 'ya' ? 'checked' : '' }} required> Ya
 							</div>
 							<div>
-								<input type="radio" name="ramah_anak" id="tidak" value="tidak" required> Tidak
+								<input type="radio" name="ramah_anak" id="tidak" value="tidak" {{ $website->ramah_anak === 'tidak' ? 'checked' : '' }} required> Tidak
 							</div>
                         </div>
                         <div class="row mt-3">
@@ -73,7 +73,7 @@
 	                    		<div class="form-group">
 	                    			<label>Nama PIC</label>
 	                    			<div>
-	                    				<input type="text" name="nama_pic" class="form-control" value="{{ old('nama_pic') }}" required>
+	                    				<input type="text" name="nama_pic" class="form-control" value="{{ $website->nama_pic }}" required>
 	                    			</div>
 	                    		</div>
 	                    	</div>
@@ -81,7 +81,7 @@
 	                    		<div class="form-group">
 	                    			<label>Jabatan</label>
 	                    			<div>
-	                    				<input type="text" name="jabatan_pic" class="form-control" value="{{ old('jabatan_pic') }}" required><br />
+	                    				<input type="text" name="jabatan_pic" class="form-control" value="{{ $website->jabatan_pic }}" required><br />
 	                    			</div>
 	                    		</div>
 	                    	</div>
@@ -89,7 +89,7 @@
 	                    		<div class="form-group">
 	                    			<label>No. Handphone</label>
 	                    			<div>
-	                    				<input type="number" name="kontak" class="form-control" value="{{ old('kontak') }}" required>
+	                    				<input type="number" name="kontak" class="form-control" value="{{ $website->kontak }}" required>
 	                    			</div>
 	                    		</div>
 	                    	</div>
