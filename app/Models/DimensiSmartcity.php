58db02c1e9490147adad74af76b86405af8e22ad
Antonius Pangaribuan <user.name>
2024-09-30 14:55:02 +0700

    buat model dimensi smartcity

---
+++ app/Models/DimensiSmartcity.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DimensiSmartcity extends Model
+{
+    use HasFactory;
+    protected $table = 'tabel_dimensi_smartcity';
+    protected $guarded = [];
+    public $incrementing = false;
+}
